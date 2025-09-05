74470104dfeaf0cb2d53b27959e7e42f9596123a
Nate Sheridan <nbs@dr.com>
2025-09-05 00:39:35 -0600

    Sound support and PWA support

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'espresso-cam-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/streamer.html',
+  '/manifest.webmanifest',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then((cached) =>
+      cached || fetch(req).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+        return res;
+      }).catch(() => cached)
+    )
+  );
+});
+
+
